19b410062351d131dfa381fb8bcc8659eb495e74
= <=>
2024-09-04 17:10:31 -0600

    Se termina la entidad Sala y se actualiza nombre para seguir con la convención de nombres.

---
+++ src/main/java/mx/gob/pjpuebla/trials/core/salas/Sala.java
@@ -0,0 +1,66 @@
+package mx.gob.pjpuebla.trials.core.salas;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import jakarta.validation.constraints.Max;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import mx.gob.pjpuebla.trials.core.bloques.Bloque;
+import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
+import mx.gob.pjpuebla.trials.util.Audit;
+import mx.gob.pjpuebla.trials.util.AuditListener;
+import mx.gob.pjpuebla.trials.util.Auditable;
+import mx.gob.pjpuebla.trials.util.Estado;
+
+@Data
+@Entity
+@EntityListeners(AuditListener.class)
+@Table(name = "TBL_SALA")
+public class Sala implements Serializable, Auditable {
+    
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idSala")
+    @SequenceGenerator(name = "idSala", sequenceName = "SEQ_SALA_ID", allocationSize = 1)
+    @Column(name = "PN_ID", insertable = false, updatable = false)
+    private Integer id; 
+
+    @Max(Integer.MAX_VALUE)
+    @Version
+    @Column(name = "N_VERSION")
+    private Integer version;
+
+    @Column(name = "S_NOMBRE")
+    private String nombre;
+
+    @Enumerated
+    @Column(name = "N_ESTADO", nullable = false)
+    private Estado estado;
+
+    @Column(name = "FN_JUEZ_ID")
+    private Integer juez; // esto debe de ser una llave foranea pero como no se tiene aun hacia que se referenciara se coloca momentaneamente como integer.
+
+    @JoinColumn(name= "FN_BLOQUE_ID", referencedColumnName = "PN_ID")
+    @OneToOne()
+    private Bloque bloque;
+    
+    @JoinColumn(name = "FN_JUZGADO_ID", referencedColumnName = "PN_ID")
+    @OneToMany()
+    private Juzgado juzgado;
+
+    @Accessors(chain = false)
+    @Embedded
+    private Audit audit;
+}
