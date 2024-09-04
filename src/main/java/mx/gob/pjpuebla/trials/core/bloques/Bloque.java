c7573e897173cc3739b95727139197431aa29d64
= <=>
2024-09-04 17:12:03 -0600

    Se termina entidad Bloque

---
+++ src/main/java/mx/gob/pjpuebla/trials/core/bloques/Bloque.java
@@ -0,0 +1,57 @@
+package mx.gob.pjpuebla.trials.core.bloques;
+
+import java.io.Serializable;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import mx.gob.pjpuebla.trials.util.Audit;
+import mx.gob.pjpuebla.trials.util.AuditListener;
+import mx.gob.pjpuebla.trials.util.Auditable;
+import mx.gob.pjpuebla.trials.util.Estado;
+
+@Data
+@Entity
+@EntityListeners(AuditListener.class)
+@Table(name = "TBL_BLOQUE")
+public class Bloque implements Serializable, Auditable{
+    
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idBloque")
+    @SequenceGenerator(name = "idBloque", sequenceName = "SEQ_BLOQUE_ID", allocationSize = 1)
+    @Column(name = "PN_ID", insertable = false, updatable = false)
+    private Integer id;
+
+    @Max(Integer.MAX_VALUE)
+    @Version
+    @Column(name = "N_VERSION")
+    private Integer version;
+
+    @NotNull
+    @Column(name = "T_HORA_INICIAL")
+    private LocalTime HoraInicial;
+
+    @NotNull
+    @Column(name = "T_HORA_FINAL")
+    private LocalTime HoraFinal;
+    
+    @Enumerated
+    @Column(name = "N_ESTADO", nullable = false)
+    private Estado estado;
+
+    @Accessors(chain = false)
+    @Embedded
+    private Audit audit;
+}
