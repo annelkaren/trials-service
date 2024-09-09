e5315749f51d854adfba3740614b2ada27f49232
= <=>
2024-09-09 00:00:23 -0600

    se agrega SetUp para bloques.

---
+++ src/test/java/mx/gob/pjpuebla/trials/core/bloques/BloqueSetUp.java
@@ -0,0 +1,27 @@
+package mx.gob.pjpuebla.trials.core.bloques;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import mx.gob.pjpuebla.trials.util.Audit;
+import mx.gob.pjpuebla.trials.util.Estado;
+
+public class BloqueSetUp {
+    
+    private BloqueSetUp(){
+
+    }
+
+    public static Bloque createBloque(){
+        Bloque bloque = new Bloque()
+                .setId(1)
+                .setEstado(Estado.ACTIVE)
+                .setHoraInicial(LocalTime.now())
+                .setHoraFinal(LocalTime.now());
+            bloque.setAudit(new Audit(LocalDateTime.now(), LocalDateTime.now(), "6b13785f-d213-4585-a76b-437ffe57c9c7",
+                "6b13785f-d213-4585-a76b-437ffe57c9c7"));
+
+        return bloque;
+    }
+
+}
