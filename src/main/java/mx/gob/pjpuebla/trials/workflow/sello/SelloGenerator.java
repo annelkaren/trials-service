5a9e0e1addb46470818e3bf481ae120ada23048b
Annel Gonzalez <annelgonzalez@MacBook-Air-de-Annel.local>
2024-09-10 06:52:48 -0600

    agregar validaciones de reimpresion

--- src/main/java/mx/gob/pjpuebla/trials/workflow/sello/SelloGenerator.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/sello/SelloGenerator.java
@@ -1,8 +1,10 @@
-package mx.gob.pjpuebla.trials.core.sello;
+package mx.gob.pjpuebla.trials.workflow.sello;
 
 import lombok.RequiredArgsConstructor;
 import mx.gob.pjpuebla.trials.core.anexos.Anexo;
+import mx.gob.pjpuebla.trials.core.anexos.AnexoRepository;
 import mx.gob.pjpuebla.trials.core.documentos.Documento;
+import mx.gob.pjpuebla.trials.core.documentos.DocumentoRepository;
 import mx.gob.pjpuebla.trials.core.personas.Persona;
 import mx.gob.pjpuebla.trials.core.personas.PersonaRepository;
 import net.sf.jasperreports.engine.*;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -28,26 +31,33 @@ public class SelloGenerator {
 
     private final PersonaRepository personaRepository;
     private final AuditorAware<Jwt> auditorAware;
+    private final DocumentoRepository documentoRepository;
+    private final AnexoRepository anexoRepository;
 
-    public byte[] exportToPdf(Documento documento,  List<Anexo> listAnexo) throws JRException, FileNotFoundException {
-        return JasperExportManager.exportReportToPdf(getReport(documento, listAnexo));
+    public byte[] exportToPdf(Integer id) throws JRException, FileNotFoundException {
+        Documento documento = documentoRepository.findById(id).orElseThrow();
+        List<Anexo> anexos = anexoRepository.findAllByDocumentoId(documento.getId());
+        return JasperExportManager.exportReportToPdf(getReport(documento, anexos));
     }
 
-    private JasperPrint getReport(Documento documento,  List<Anexo>  listAnexo) throws FileNotFoundException, JRException {
-        String codeVerificacion = generateVerificationString(documento, listAnexo);
+    private JasperPrint getReport(Documento documento, List<Anexo> anexos) throws FileNotFoundException, JRException {
+        String date = getDate();
+        String verificationCode = generateVerificationCode(documento, anexos, date);
 
-        List<String> listAnexoStrings = listAnexo.stream()
+        List<String> listAnexoStrings = anexos.stream()
                 .map(Anexo::getNombre)
                 .toList();
+
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("expediente", documento.getExpediente());
-        parameters.put("fechaHoraRecepcion", "");
+        parameters.put("fechaHoraRecepcion", date);
         parameters.put("folio", documento.getFolio());
         parameters.put("anexos", listAnexoStrings);
-        parameters.put("cadenaVerificacion", codeVerificacion);
+        parameters.put("cadenaVerificacion", verificationCode);
         parameters.put("nombreEntidad", "PENDIENTE");
         parameters.put("nombreJuzgado", documento.getJuzgado().getNombre());
         parameters.put("capturista ", getCapturista());
+        parameters.put("reimpresion ", isReimpresion(documento.getAudit().getUsuarioAlta(), documento.getAudit().getFechaAlta()));
         parameters.put("marcaAgua", "src/main/resources/jasper/escudo.png");
         parameters.put("logotipoHeder", "src/main/resources/jasper/header.jpg");
 
@@ -57,30 +67,45 @@ public class SelloGenerator {
     }
 
     private String getCapturista() {
-        String usuario = auditorAware.getCurrentAuditor().get().getSubject();
-        Persona persona = personaRepository.findByUsuario(usuario);
+        Jwt jwt = auditorAware.getCurrentAuditor().orElseThrow();
+        String user = jwt.getSubject();
+        Persona persona = personaRepository.findByUsuario(user);
         String apellidoMaterno = persona.getApellidoMaterno();
         apellidoMaterno = (apellidoMaterno != null && !apellidoMaterno.isEmpty()) ? String.valueOf(apellidoMaterno.charAt(0)) : "";
         return persona.getNombre().charAt(0) + "" + persona.getApellidoPaterno().charAt(0) + apellidoMaterno;
     }
 
-    public String generateVerificationString(Documento documento,List<Anexo>  listAnexo ) {
-        LocalDateTime fechaActual = LocalDateTime.now();
+    private String getDate() {
+        LocalDateTime today = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String fechaActualString = fechaActual.format(formatter);
+        return today.format(formatter);
+    }
 
-        String listaNombres = listAnexo.stream()
-                .map(Anexo::getNombre)
-                .collect(Collectors.joining(","));
+    private boolean isReimpresion(String auditor, LocalDateTime date) {
+        boolean isSameAuditor, isSameDate = false;
+        LocalDate today = LocalDate.now();
+        Jwt jwt = auditorAware.getCurrentAuditor().orElseThrow();
+        String currentUser = jwt.getSubject();
+        isSameAuditor = (auditor.equals(currentUser));
+        isSameDate = (date.toLocalDate().equals(today));
+        return !(isSameAuditor && isSameDate);
+    }
 
-        String concatenatedString = String.join("|",
+    public String generateVerificationCode(Documento documento, List<Anexo> anexos, String date) {
+        String verificationStringCode = String.join("|",
                 documento.getJuzgado().getNombre(),
                 documento.getExpediente(),
                 documento.getFolio(),
-                fechaActualString,
-                listaNombres
+                date,
+                getAnexos(anexos)
         );
-        return Base64.getEncoder().encodeToString(concatenatedString.getBytes());
+        return Base64.getEncoder().encodeToString(verificationStringCode.getBytes());
+    }
+
+    private String getAnexos(List<Anexo> anexos) {
+        return anexos.stream()
+                .map(Anexo::getNombre)
+                .collect(Collectors.joining(","));
     }
 
 }
