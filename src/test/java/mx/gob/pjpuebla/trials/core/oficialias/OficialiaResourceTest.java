7b8b8b4ab7f1028caabeb9abe0d3a914cdcac216
Daniel-Fernandez-CS <156028131+Daniel-Fernandez-CS@users.noreply.github.com>
2024-09-11 12:07:05 -0600

    Realizando tests

--- src/test/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaResourceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaResourceTest.java
@@ -1,7 +1,17 @@
 package mx.gob.pjpuebla.trials.core.oficialias;
 
 import jakarta.ws.rs.core.MediaType;
-import mx.gob.pjpuebla.trials.core.materias.*;
+import mx.gob.pjpuebla.trials.core.distritos.Distrito;
+import mx.gob.pjpuebla.trials.core.distritos.DistritoSetUp;
+import mx.gob.pjpuebla.trials.core.domicilio.DomicilioSetUp;
+import mx.gob.pjpuebla.trials.core.domicilios.Domicilio;
+import mx.gob.pjpuebla.trials.core.oficialias.*;
+import mx.gob.pjpuebla.trials.core.sedes.Sede;
+import mx.gob.pjpuebla.trials.core.sedes.SedeRecordResponse;
+import mx.gob.pjpuebla.trials.core.sedes.SedeSetUp;
+import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialia;
+import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialiaRecord;
+import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialiaSetUp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,32 +32,40 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(MateriaResource.class)
+@WebMvcTest(OficialiaResource.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
 class OficialiaResourceTest {
 
     @MockBean
-    private MateriaService mockMateriaService;
+    private OficialiaService mockOficialiaService;
 
     @Autowired
     private MockMvc mockMvc;
 
-    private MateriaRecord validMateriaRecord;
+    private Oficialia oficialia;
+    private OficialiaRecord validOficialiaRecord;
 
     @BeforeEach
     void setUp() {
-        validMateriaRecord = MateriaSetUp.createMateriaRecord();
+        TipoOficialia tipoOficialia = TipoOficialiaSetUp.createtipoOficialia();
+        Distrito distrito = DistritoSetUp.createDistrito();
+        Domicilio domicilio = DomicilioSetUp.createDomicilio();
+        Sede sede = SedeSetUp.createSede();
+        sede.setDistrito(distrito);
+        sede.setDomicilio(domicilio);
+        oficialia = OficialiaSetUp.createOficialia(tipoOficialia, sede);
+        validOficialiaRecord = OficialiaSetUp.createOficialiaRecord(oficialia, new TipoOficialiaRecord(tipoOficialia.getId(), tipoOficialia.getNombre()), new SedeRecordResponse(sede.getId(),sede.getNombre(),sede.getEstado()));
     }
 
     @Test
     void getAllByNameAndActive_success() throws Exception {
-        given(mockMateriaService.getAllActive(any(Pageable.class), any(Materia.class)))
-                .willReturn(new PageImpl<>(Collections.singletonList(validMateriaRecord)));
+        given(mockOficialiaService.getAllActive(any(Pageable.class), any(Oficialia.class)))
+                .willReturn(new PageImpl<>(Collections.singletonList(validOficialiaRecord)));
 
         mockMvc.perform(
-                get("/api/core/materias")
-                        .param("materiaName", "PE")
+                get("/api/core/oficialias")
+                        .param("oficialiaName", "O")
                         .accept(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk());
     }
---
+++ src/test/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaSetUp.java
@@ -0,0 +1,37 @@
+package mx.gob.pjpuebla.trials.core.oficialias;
+
+import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
+import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRecord;
+import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRecordResponse;
+import mx.gob.pjpuebla.trials.core.sedes.Sede;
+import mx.gob.pjpuebla.trials.core.sedes.SedeRecordResponse;
+import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialia;
+import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialiaRecord;
+import mx.gob.pjpuebla.trials.util.Audit;
+import mx.gob.pjpuebla.trials.util.enums.Estado;
+
+import java.time.LocalDateTime;
+
+public class OficialiaSetUp {
+
+    private OficialiaSetUp() {
+    }
+
+    public static Oficialia createOficialia(TipoOficialia tipoOficialia, Sede sede) {
+        Oficialia oficialia = new Oficialia()
+                .setId(1)
+                .setVersion(0)
+                .setEstado(Estado.ACTIVE)
+                .setTipo(tipoOficialia)
+                .setNombre("Comun")
+                .setResponsable("Responsable")
+                .setSede(sede);
+        oficialia.setAudit(new Audit(LocalDateTime.now(), LocalDateTime.now(), "6b13785f-d213-4585-a76b-437ffe57c9c7", "6b13785f-d213-4585-a76b-437ffe57c9c7"));
+        return oficialia;
+    }
+
+    public static OficialiaRecord createOficialiaRecord(Oficialia oficialia, TipoOficialiaRecord tipo, SedeRecordResponse sede) {
+        return new OficialiaRecord(oficialia.getId(), oficialia.getVersion(), Estado.ACTIVE, tipo, oficialia.getNombre(), oficialia.getResponsable(), sede);
+    }
+
+}
