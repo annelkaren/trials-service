04f6dcf576d9e56349268dee317b904ea4dc045b
jnsrjzgo <jnsrjzgo@icloud.com>
2024-09-12 23:51:11 -0600

    pruebas digitalizacion

--- src/test/java/mx/gob/pjpuebla/trials/workflow/files/DigitalizacionFolderServiceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/workflow/files/DigitalizacionFolderServiceTest.java
@@ -1,24 +1,5 @@
 package mx.gob.pjpuebla.trials.workflow.files;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import lombok.extern.slf4j.Slf4j;
 import mx.gob.pjpuebla.trials.core.distritos.Distrito;
 import mx.gob.pjpuebla.trials.core.distritos.DistritoRepository;
@@ -43,11 +24,28 @@ import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistema;
 import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistemaRepository;
 import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistemaSetUp;
 import mx.gob.pjpuebla.trials.util.TipoDocumento;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @Slf4j
 @SpringBootTest
 public class DigitalizacionFolderServiceTest {
 
+    public static final String FILE_PATH = "/opt/pjp/files/digitalizacion/2024/Juzgado/000001";
     @Mock
     private SedeRepository sedeRepository;
     @Mock
@@ -61,83 +59,33 @@ public class DigitalizacionFolderServiceTest {
     @Mock
     private TipoSistemaRepository tipoSistemaRepository;
 
-    private Documento documento;
-    private Juzgado juzgado;
-    private Path testFolderPath;
- 
-    @Value("${root-folder}")
+    @Value("${app.root-folder}")
     private String rootFolder;
 
     @InjectMocks
     private DigitalizacionFolderService digitalizacionFolderService;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        // Configuración de los mocks
-        Materia materia = MateriaSetUp.createMateria();
-        Distrito distrito = DistritoSetUp.createDistrito();
-        Domicilio domicilio = DomicilioSetUp.createDomicilio();
-        Sede sede = SedeSetUp.createSede();
-        TipoSistema tipoSistema = TipoSistemaSetUp.createTipoSistema();
-        TipoJuicio tipoJuicio = TipoJuicioSetUp.createTipoJuicio(tipoSistema, materia);
-
-        given(materiaRepository.save(materia)).willReturn(materia);
-        given(distritoRepository.save(distrito)).willReturn(distrito);
-        given(domicilioRepository.save(domicilio)).willReturn(domicilio);
-        given(sedeRepository.save(sede)).willReturn(sede);
-        given(tipoSistemaRepository.save(tipoSistema)).willReturn(tipoSistema);
-        given(tipoJuicioRepository.save(tipoJuicio)).willReturn(tipoJuicio);
-
-        sede.setDistrito(distrito);
-        sede.setDomicilio(domicilio);
-
-        juzgado = JuzgadoSetUp.createJuzgado(materia, sede);
-        documento = DocumentoTestSetUp.create(TipoDocumento.DEMANDA, tipoJuicio, juzgado);
-        System.out.println("EL VALOR INICIAL DE LA PRUEBA UNINTARIA ES: " + rootFolder);
-        testFolderPath = Paths.get(rootFolder, "digitalizacion", "2024", "Juzgado", "000001");
-    }
-
     @Test
-    void createFolder() throws IOException {
+    void createFolder() {
+        given(materiaRepository.save(any(Materia.class))).willReturn(MateriaSetUp.createMateria());
+        given(distritoRepository.save(any(Distrito.class))).willReturn(DistritoSetUp.createDistrito());
+        given(domicilioRepository.save(any(Domicilio.class))).willReturn(DomicilioSetUp.createDomicilio());
+        given(sedeRepository.save(any(Sede.class))).willReturn(SedeSetUp.createSede());
+        given(tipoSistemaRepository.save(any(TipoSistema.class))).willReturn(TipoSistemaSetUp.createTipoSistema());
+        given(tipoJuicioRepository.save(any(TipoJuicio.class))).willReturn(TipoJuicioSetUp.createTipoJuicio());
 
-        String rutaCarpeta = digitalizacionFolderService.createFolderDigitalizacion(documento);
-        System.out.println("comprobare que se creo la carpeta: "+testFolderPath);
-        assertThat(Files.exists(testFolderPath)).isTrue();
-        assertThat(rutaCarpeta).isEqualTo(testFolderPath.toString());
-    }
+        ReflectionTestUtils.setField(digitalizacionFolderService, "rootFolder", rootFolder);
+        Juzgado juzgado = JuzgadoSetUp.createJuzgado();
+        Documento documento = DocumentoTestSetUp.create(TipoDocumento.DEMANDA, TipoJuicioSetUp.createTipoJuicio(), juzgado);
 
-    @AfterEach
-    public void tearDown() throws IOException {
-        try {
-            Path rootFolderPath = Paths.get(rootFolder, "digitalizacion");
-    
-            if (Files.exists(rootFolderPath)) {
-               
-                Files.walkFileTree(rootFolderPath, new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        Files.delete(file);  
-                        return FileVisitResult.CONTINUE;
-                    }
-    
-                    @Override
-                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                        Files.delete(dir); 
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-                log.info("Carpeta 'digitalizacion' y su contenido fueron eliminados exitosamente.");
-            } else {
-                log.warn("La carpeta raíz 'digitalizacion' no existe.");
-            }
-        } catch (IOException e) {
-            log.error("Error al eliminar las carpetas de digitalización: {}", e.getMessage(), e);
-            throw new RuntimeException("Error al eliminar las carpetas de digitalización", e);
+        try (MockedStatic<Files> files = Mockito.mockStatic(Files.class)) {
+            files.when(() -> Files.createDirectories(any(Path.class)))
+                    .thenReturn(Paths.get(FILE_PATH));
+
+            String rutaCarpeta = digitalizacionFolderService.createFolderDigitalizacion(documento);
+            assertThat(rutaCarpeta).isEqualToIgnoringCase(FILE_PATH);
         }
+
     }
-    
-    
-    
+
 }
