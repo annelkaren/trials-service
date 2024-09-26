7e4129efc64b18f9ce33cd4b0c27d8c6dab33f84
Alex celisGuz <celisalex77@gmail.com>
2024-09-26 12:05:12 -0600

    Materia Oficialias service 2

--- src/test/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaRepositoryTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaRepositoryTest.java
@@ -6,12 +6,24 @@ import mx.gob.pjpuebla.trials.core.distritos.DistritoSetUp;
 import mx.gob.pjpuebla.trials.core.domicilio.DomicilioSetUp;
 import mx.gob.pjpuebla.trials.core.domicilios.Domicilio;
 import mx.gob.pjpuebla.trials.core.domicilios.DomicilioRepository;
+import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
+import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRepository;
+import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoSetUp;
+import mx.gob.pjpuebla.trials.core.materias.Materia;
+import mx.gob.pjpuebla.trials.core.materias.MateriaRepository;
+import mx.gob.pjpuebla.trials.core.materias.MateriaSetUp;
 import mx.gob.pjpuebla.trials.core.sedes.Sede;
 import mx.gob.pjpuebla.trials.core.sedes.SedeRepository;
 import mx.gob.pjpuebla.trials.core.sedes.SedeSetUp;
+import mx.gob.pjpuebla.trials.core.tipojuicio.TipoJuicio;
+import mx.gob.pjpuebla.trials.core.tipojuicio.TipoJuicioRepository;
+import mx.gob.pjpuebla.trials.core.tipojuicio.TipoJuicioSetUp;
 import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialia;
 import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialiaRepository;
 import mx.gob.pjpuebla.trials.core.tipooficialias.TipoOficialiaSetUp;
+import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistema;
+import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistemaRepository;
+import mx.gob.pjpuebla.trials.core.tiposistema.TipoSistemaSetUp;
 import mx.gob.pjpuebla.trials.core.utils.audit.AuditConfigTest;
 import mx.gob.pjpuebla.trials.util.enums.Estado;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,20 +54,39 @@ class OficialiaRepositoryTest extends AuditConfigTest {
     @Autowired
     private SedeRepository sedeRepository;
     @Autowired
+    private JuzgadoRepository juzgadoRepository;
+    @Autowired
+    private MateriaRepository materiaRepository;
+    @Autowired
+    private TipoJuicioRepository tipoJuicioRepository;
+    @Autowired
+    private TipoSistemaRepository tipoSistemaRepository;
+
+    @Autowired
     private TipoOficialiaRepository tipoOficialiaRepository;
     private Oficialia oficialia;
 
     @BeforeEach
     void setUp() {
         TipoOficialia tipoOficialia = TipoOficialiaSetUp.createtipoOficialia();
+        Materia materia = MateriaSetUp.createMateria();
+        materia = materiaRepository.save(materia);
         tipoOficialia = tipoOficialiaRepository.save(tipoOficialia);
         Distrito distrito = distritoRepository.save(DistritoSetUp.createDistrito());
         Domicilio domicilio = domicilioRepository.save(DomicilioSetUp.createDomicilio());
+
+        TipoSistema tipoSistema = tipoSistemaRepository.save(TipoSistemaSetUp.createTipoSistema());
+        TipoJuicio tipoJuicio = tipoJuicioRepository.save(TipoJuicioSetUp.createTipoJuicio(tipoSistema, materia));
+
         Sede sede = SedeSetUp.createSede();
         sede.setDistrito(distrito);
         sede.setDomicilio(domicilio);
         sede = sedeRepository.save(sede);
+        Juzgado juzgado = JuzgadoSetUp.createJuzgado(materia,sede);
+        juzgado.setTipoJuicios(List.of(tipoJuicio));
+        juzgado = juzgadoRepository.save(juzgado);
         oficialia = OficialiaSetUp.createOficialia(tipoOficialia, sede);
+        oficialia.setJuzgado(juzgado);
     }
 
     @Test
