70b49c6135a4557491d1dd34e8b6c04fd80f18c1
= <=>
2024-10-03 12:37:02 -0600

    prueba de resource añadida.

--- src/main/java/mx/gob/pjpuebla/trials/core/tipojuicio/TipoJuicioResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/core/tipojuicio/TipoJuicioResource.java
@@ -12,7 +12,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
