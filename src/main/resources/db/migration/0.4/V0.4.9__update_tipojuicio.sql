27d234d27c77c3edb05635d1702b302683e4607a
= <=>
2024-10-03 16:39:09 -0600

    corrección sql.

--- src/main/resources/db/migration/0.4/V0.4.9__update_tipojuicio.sql
+++ src/main/resources/db/migration/0.4/V0.4.9__update_tipojuicio.sql
@@ -1,3 +1,3 @@
-UPDATE TBL_TIPO_JUICIO
-SET NOMBRE = "Familiar Oralidad (Divorcio Incausado Bilateral)"
+UPDATE TRIALS.TBL_TIPO_JUICIO
+SET S_NOMBRE = "Familiar Oralidad (Divorcio Incausado Bilateral)"
 WHERE PN_ID = 113;
