a8b3259bd8e93caedaf620630654580dd6d48d7a
= <=>
2024-10-03 14:06:03 -0600

    Se eliminan imports no utilizados.

--- src/main/java/mx/gob/pjpuebla/trials/workflow/personasdocumentos/PersonaDocumentoItemRecord.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/personasdocumentos/PersonaDocumentoItemRecord.java
@@ -1,7 +1,5 @@
 package mx.gob.pjpuebla.trials.workflow.personasdocumentos;
 
-import java.util.List;
-
 public record PersonaDocumentoItemRecord(
         String nombre,
         String apellidoPaterno,
