7b6cfcf0bce3b0fc7c4bc85599b83ff920930286
David Pastrana <david.pastrana.aco@gmail.com>
2024-10-04 09:17:00 -0600

    cambio en endpoint para consultar expediente y juzgado en carpeta

--- src/main/java/mx/gob/pjpuebla/trials/workflow/carpeta/records/CarpetaSearchRecord.java
+++
@@ -1,9 +0,0 @@
-package mx.gob.pjpuebla.trials.workflow.carpeta.records;
-
-import java.io.Serializable;
-
-public record CarpetaSearchRecord(
-        String numExpediente,
-        Integer year,
-        Integer idJuzgado
-) implements Serializable {}
