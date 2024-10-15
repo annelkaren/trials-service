c9cfe384f342f4954e51821177f6f88be9d7d7d1
Alejandro Ramírez <alejandro.ramirezc@gmail.com>
2024-10-14 22:34:27 -0600

    Corrige sintaxis

--- src/main/resources/db/migration/0.5/V0.5.7__create_documento_folio.sql
+++ src/main/resources/db/migration/0.5/V0.5.7__create_documento_folio.sql
@@ -10,6 +10,6 @@ CREATE TABLE trials.TBL_DOCUMENTO_FOLIO (
 	n_tipo_centro_trabajo int NOT NULL,
 	n_folio int DEFAULT 1 NOT NULL,
 	n_year int DEFAULT EXTRACT('YEAR' FROM CURRENT_DATE) NOT NULL,
-	PRIMARY KEY(pn_id)
+	PRIMARY KEY(pn_id),
 	CONSTRAINT tbl_documento_folio_unique UNIQUE (n_tipo_documento,n_centro_trabajo_id,n_tipo_centro_trabajo, n_year)
 );
