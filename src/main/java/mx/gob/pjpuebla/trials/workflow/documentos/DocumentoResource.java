e68d0c78ff0aa51fd4f2a59618437d10833821d2
Alexis <alexxarellan@hotmail.com>
2024-10-26 12:01:56 -0600

    SE ELIMINA IMPORT

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoResource.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import org.springframework.web.bind.annotation.PutMapping;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
