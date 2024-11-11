92def614a97ad2dada2f4959459c010775f82818
Annel Gonzalez <annel.gonzalez1@outlook.com>
2024-11-11 10:10:03 -0600

    eliminar import no utilizado

--- src/main/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaRepository.java
+++ src/main/java/mx/gob/pjpuebla/trials/core/oficialias/OficialiaRepository.java
@@ -1,6 +1,5 @@
 package mx.gob.pjpuebla.trials.core.oficialias;
 
-import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRecordItem;
 import mx.gob.pjpuebla.trials.util.enums.Estado;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
