eddea231299bd393f5be0db3e44849c7ccf6de23
Annel Gonzalez <annel.gonzalez1@outlook.com>
2024-11-14 20:01:31 -0600

    actualizar test

--- src/test/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoServiceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoServiceTest.java
@@ -259,10 +259,12 @@ class DocumentoServiceTest {
         demanda.getCarpeta().setFolio("1");
         demanda.getCarpeta().setJuzgado(juzgado);
         demanda.getCarpeta().getJuzgado().setMateria(MateriaSetUp.createMateria());
+        Movimiento movimiento = new Movimiento().setDocumento(demanda);
 
-        List<Documento> listPage = Collections.singletonList(demanda);
-        
-        given(documentoRepository.findByEstatusCaptura(any(String.class), any(PageRequest.class)))
+        List<Movimiento> listPage = Collections.singletonList(movimiento);
+
+        given(personaService.getAuditor()).willReturn(new Persona().setJuzgado(juzgado));
+        given(movimientoService.getAllBandejaEntrada(any(),any(),any(), any()))
                 .willReturn(new PageImpl<>(listPage, PageRequest.of(0, listPage.size()), listPage.size()));
         
         Page<DocumentoGridRecord> page = documentoService.getAll(demanda.getCarpeta().getFolio(),
@@ -953,7 +955,7 @@ class DocumentoServiceTest {
                 .first()
                 .hasFieldOrPropertyWithValue("id", documentoAsignadoRecord.id())
                 .hasFieldOrPropertyWithValue("expediente", documentoAsignadoRecord.expediente())
-                .hasFieldOrPropertyWithValue("tipoEntrada", documentoAsignadoRecord.tipoCarpeta().name())
+                .hasFieldOrPropertyWithValue("tipoEntrada", documentoAsignadoRecord.tipoDocumento().name())
                 .hasFieldOrPropertyWithValue("concepto", documentoAsignadoRecord.concepto().getNombre())
                 .hasFieldOrPropertyWithValue("fechaTurnado", documentoAsignadoRecord.fechaTurnado())
                 .hasFieldOrPropertyWithValue("fechaTermino", documentoAsignadoRecord.fechaTurnado().plusDays(concepto.getDias()));
