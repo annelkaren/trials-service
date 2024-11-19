cc43dc3d0d512564d1cd34e340d4f65c87bb7350
Eduardo <eadweard132@gmail.com>
2024-11-18 18:35:52 -0600

    comentarios resueltos

--- src/main/java/mx/gob/pjpuebla/trials/workflow/audiencias/AudienciaResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/audiencias/AudienciaResource.java
@@ -6,7 +6,6 @@ import mx.gob.pjpuebla.trials.workflow.audiencias.record.AudienciasGeneralesResp
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
