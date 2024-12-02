732f89bd10536d8ecb2b51ebeddadd52285ff9bb
David Acosta <drks_2012@hotmail.com>
2024-12-02 15:06:27 +0000

    Merged PR 651: 482-David-Add_column_bandeja_entrada-Back
    Se añade organojurisdiccional en bandeja de entrada
    
    Related work items: #482

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
@@ -135,8 +135,9 @@ public class DocumentoService {
                             documento.getAudit().getFechaAlta(),
                             carpeta.getSelloEstatus(),
                             (documento.getTipoDocumento() != null) ? documento.getEstatus() : carpeta.getEstatus(),
-                            (documento.getRuta() != null));
-
+                            (documento.getRuta() != null),
+                            documento.getCarpeta().getJuzgado().getNombre()
+                    );
             list.add(documentoGridRecord);
         }
         return new PageImpl<>(list, pageable, page.getTotalElements());
@@ -418,7 +419,8 @@ public class DocumentoService {
                     movimiento.getFechaAsignacion(),
                     null,
                     EstadoCarpeta.valueOf(movimiento.getEstado()),
-                    false
+                    false,
+                    ""
             );
             listaDocumentoRecords.add(drecord);
         }
