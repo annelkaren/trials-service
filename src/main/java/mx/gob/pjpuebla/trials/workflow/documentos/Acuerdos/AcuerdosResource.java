76180d329facb9d5953fe21510385dd27f53f356
Alex celisGuz <celisalex77@gmail.com>
2024-12-09 18:36:58 -0600

    Ver_PDF_acuerdo

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/Acuerdos/AcuerdosResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/Acuerdos/AcuerdosResource.java
@@ -88,8 +88,9 @@ public class AcuerdosResource {
         return acuerdosService.update(acuerdo);
     }
 
-    @GetMapping(value = "/acuerdos/{documentoId}", produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/documentos/acuerdos/{documentoId}", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<byte[]> exportAcuerdoPdf(@PathVariable Integer documentoId) throws JRException, IOException, WriterException {
+        System.out.println(documentoId);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.setContentDispositionFormData("acuerdo", documentoId + "_Documento.pdf");
