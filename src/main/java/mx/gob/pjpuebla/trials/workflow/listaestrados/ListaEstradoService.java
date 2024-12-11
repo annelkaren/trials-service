6f581c37fba199f75790a35ac560955ab9fd8ba7
Annel Gonzalez <annel.gonzalez1@outlook.com>
2024-12-11 07:37:05 -0600

    permisos

--- src/main/java/mx/gob/pjpuebla/trials/workflow/listaestrados/ListaEstradoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/listaestrados/ListaEstradoService.java
@@ -87,10 +87,10 @@ public class ListaEstradoService {
                 .map(notificacion -> {
                     String juzgado = nombreCentroTrabajo != null ? nombreCentroTrabajo : "";
                     String diaPublicado = LocalDate.now().toString();
-                    String asunto = "EXP." + notificacion.getCarpeta().getExpediente()
-                            + "\n" + notificacion.getCarpeta().getTipoJuicio().getNombre()
+                    String asunto = "EXP." + notificacion.getDocumento().getCarpeta().getExpediente()
+                            + "\n" + notificacion.getDocumento().getCarpeta().getTipoJuicio().getNombre()
                             + "\n" + "***** VS *****";
-                    String nombresRubros = notificacion.getCarpeta().getRubros().stream()
+                    String nombresRubros = notificacion.getDocumento().getCarpeta().getRubros().stream()
                             .map(Rubro::getNombre)
                             .collect(Collectors.joining(", "));
                     DocumentoDetalle doc = documentoDetalleRepository.findByDocumentoId(notificacion.getDocumento().getId()).orElse(null);
