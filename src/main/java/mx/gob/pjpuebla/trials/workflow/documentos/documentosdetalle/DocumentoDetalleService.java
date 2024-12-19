e3379df3ea0f368d07b181e715f6031781ac237e
alexis <alexxarellan@hotmail.com>
2024-12-19 12:27:22 -0600

    se ajusta publicacion de oficio y upload de acuse (se coloca ahora el estatus segun la accion)

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/documentosdetalle/DocumentoDetalleService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/documentosdetalle/DocumentoDetalleService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mx.gob.pjpuebla.trials.util.enums.EstadoCarpeta;
 import mx.gob.pjpuebla.trials.workflow.documentos.DigitalizacionService;
+import mx.gob.pjpuebla.trials.workflow.documentos.Documento;
+import mx.gob.pjpuebla.trials.workflow.documentos.DocumentoRepository;
 import mx.gob.pjpuebla.trials.workflow.documentos.documentosdetalle.records.DocumentoDetalleRecord;
 import mx.gob.pjpuebla.trials.workflow.documentos.records.DigitalizacionRecord;
 import java.io.IOException;
@@ -17,13 +20,17 @@ import java.io.IOException;
 public class DocumentoDetalleService {
     private final DocumentoDetalleRepository documentoDetalleRepository;
     private final DigitalizacionService digitalizacion2Service;
+    private final DocumentoRepository documentoRepository;
 
     @Value("${app.root-folder}")
     private String rootFolder;
 
     public DigitalizacionRecord digitalizacionAcuse(DocumentoDetalleRecord documento) {
+        Documento doc = documentoRepository.findById(documento.documentoId()).orElse(null);
         DocumentoDetalle docDetalle = documentoDetalleRepository.findByDocumentoId(documento.documentoId()).orElse(null);
         DigitalizacionRecord digitalizacion = digitalizacion2Service.guardarArchivo(documento.file(), documento.documentoId());
+
+
 
         if (docDetalle != null) {
             docDetalle.setRuta(documento.file().getOriginalFilename());
@@ -31,6 +38,9 @@ public class DocumentoDetalleService {
             docDetalle.setComentario(documento.comentario());
             docDetalle.setFechaEntrega(documento.fechaEntrega());
             documentoDetalleRepository.save(docDetalle);
+
+            doc.setEstatus(EstadoCarpeta.CON_ACUSE);
+            documentoRepository.save(doc);
         }
 
         return digitalizacion;
