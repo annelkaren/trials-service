41865c49218543f80f5d367f05e76f02cdbf8e78
Alex celisGuz <celisalex77@gmail.com>
2024-12-28 18:16:43 -0600

    Ajustes de imports cuestionarioservice

--- src/main/java/mx/gob/pjpuebla/trials/core/cuestionarios/CuestionarioService.java
+++ src/main/java/mx/gob/pjpuebla/trials/core/cuestionarios/CuestionarioService.java
@@ -3,12 +3,10 @@ package mx.gob.pjpuebla.trials.core.cuestionarios;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mx.gob.pjpuebla.trials.util.enums.ListCuestionario;
-import mx.gob.pjpuebla.trials.util.enums.TipoDerechosHumanos;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
