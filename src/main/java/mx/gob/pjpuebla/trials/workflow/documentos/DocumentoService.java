97e0e053238421c69dd7e7071dfdfbfb67afcaf1
Alejandro Ramírez <alejandro.ramirez@pjpuebla.gob.mx>
2025-01-03 15:44:29 -0600

    Filtra los juzgados por tipo de juicio

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
@@ -216,12 +216,15 @@ public class DocumentoService {
         if (oficialia == null) {
             throw new NotFoundException("La persona no está relacionada con ninguna oficialía", "persona.getOficialia()");
         }
-        List<Juzgado> juzgadosRelacionados = juzgadoRepository.findJuzgadoByOficialiaId(oficialia.getId());
+        
         Documento documento = new Documento();
         Carpeta carpeta = new Carpeta();
         TipoJuicio tipoJuicio = tipoJuicioRepository.findById(documentoRecord.tipoJuicioId())
                 .orElseThrow(() -> new NotFoundException(TIPO_JUICIO_NOT_FOUND, documentoRecord.tipoJuicioId().toString()));
         carpeta.setTipoJuicio(tipoJuicio);
+
+        List<Juzgado> juzgadosRelacionados = juzgadoRepository.findJuzgadoByOficialiaId(oficialia.getId())
+            .stream().filter(j->j.getTipoJuicios().contains(tipoJuicio)).toList();
 
         Juzgado juzgadoConexidad = juzgadoService.getConexidadJuzgado(documentoRecord.actor(), documentoRecord.demandado(), carpeta.getTipoJuicio());
         if (juzgadoConexidad != null && !juzgadosRelacionados.contains(juzgadoConexidad)) {
