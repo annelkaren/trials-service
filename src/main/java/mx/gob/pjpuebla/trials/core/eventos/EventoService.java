b7d685e6ad194fbbd691caaa8732658d3ce12464
Daniel-Fernandez-CS <156028131+Daniel-Fernandez-CS@users.noreply.github.com>
2025-01-13 01:02:22 -0600

    Periodos vacacionales y dias inhabiles

--- src/main/java/mx/gob/pjpuebla/trials/core/eventos/EventoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/core/eventos/EventoService.java
@@ -1,9 +1,16 @@
 package mx.gob.pjpuebla.trials.core.eventos;
 
 import lombok.AllArgsConstructor;
+import mx.gob.pjpuebla.trials.core.eventos.records.EventoEditRecord;
+import mx.gob.pjpuebla.trials.core.eventos.records.EventoPeriodosRecord;
 import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
 import mx.gob.pjpuebla.trials.core.oficialias.Oficialia;
+import mx.gob.pjpuebla.trials.core.personas.Persona;
+import mx.gob.pjpuebla.trials.core.personas.PersonaService;
+import mx.gob.pjpuebla.trials.error.NotFoundException;
 import mx.gob.pjpuebla.trials.util.FinSemana;
+import mx.gob.pjpuebla.trials.util.enums.Estado;
+import mx.gob.pjpuebla.trials.workflow.carpeta.Carpeta;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -19,6 +26,7 @@ import java.time.DateTimeException;
 @AllArgsConstructor
 public class EventoService {
     private EventoRepository eventoRepository;
+    private final PersonaService personaService;
 
     @Transactional(readOnly = true)
     public Page<EventoRecord> getAll(Evento example, Pageable pageable) {
@@ -73,5 +81,127 @@ public class EventoService {
         }
 
         return siguienteDia;
+    }
+
+    public Evento createPeriodos(EventoPeriodosRecord record){
+
+        if (record.diaInicio().isAfter(record.diaFin()))
+            throw new DateTimeException("La fecha de inicio no puede ser mayor a la fecha de finalización");
+
+        Persona persona = personaService.getAuditor();
+        Evento evento = null;
+
+        switch (record.calendarios()) {
+            case "General" -> {
+                evento = new Evento()
+                        .setDescripcion(record.descripcion())
+                        .setDiaInicio(record.diaInicio())
+                        .setDiaFin(record.diaFin())
+                        .setEstado(Estado.ACTIVE);
+                eventoRepository.save(evento);
+            }
+            case "Oficialía Común" -> {
+                evento = new Evento()
+                        .setDescripcion(record.descripcion())
+                        .setDiaInicio(record.diaInicio())
+                        .setDiaFin(record.diaFin())
+                        .setEstado(Estado.ACTIVE);
+                if (persona.getOficialia() != null) evento.setOficialia(persona.getOficialia());
+                if (persona.getJuzgado() != null) evento.setJuzgado(persona.getJuzgado());
+                eventoRepository.save(evento);
+            }
+            case "General-Oficialía Común" -> {
+                evento = new Evento()
+                        .setDescripcion(record.descripcion())
+                        .setDiaInicio(record.diaInicio())
+                        .setDiaFin(record.diaFin())
+                        .setEstado(Estado.ACTIVE);
+                eventoRepository.save(evento);
+
+                Evento evento2 = new Evento()
+                        .setDescripcion(record.descripcion())
+                        .setDiaInicio(record.diaInicio())
+                        .setDiaFin(record.diaFin())
+                        .setEstado(Estado.ACTIVE);
+                if (persona.getOficialia() != null) evento2.setOficialia(persona.getOficialia());
+                if (persona.getJuzgado() != null) evento2.setJuzgado(persona.getJuzgado());
+                eventoRepository.save(evento2);
+            }
+        }
+
+        assert evento != null;
+        return eventoRepository.save(evento);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EventoRecord> getEventosGenerales(Pageable pageable) {
+        Page<Evento> eventosPage = eventoRepository.findEventosGenerales(pageable);
+
+        List<EventoRecord> eventoRecords = eventosPage.getContent().stream()
+                .map(evento -> new EventoRecord(
+                        evento.getId(),
+                        evento.getDiaInicio(),
+                        evento.getDiaFin(),
+                        evento.getDescripcion(),
+                        evento.getJuzgado() != null ? evento.getJuzgado().getNombre() : null,
+                        evento.getOficialia() != null ? evento.getOficialia().getNombre() : null
+                ))
+                .toList();
+
+        return new PageImpl<>(eventoRecords, pageable, eventosPage.getTotalElements());
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EventoRecord> getEventosOficialiaComun(Pageable pageable) {
+        Persona persona = personaService.getAuditor();
+        Oficialia oficialia = persona.getOficialia();
+        Juzgado juzgado = persona.getJuzgado();
+
+        Page<Evento> eventosPage = eventoRepository.findByOficialiaOrJuzgado(oficialia, juzgado, pageable);
+
+        List<EventoRecord> eventoRecords = eventosPage.getContent().stream()
+                .map(evento -> new EventoRecord(
+                        evento.getId(),
+                        evento.getDiaInicio(),
+                        evento.getDiaFin(),
+                        evento.getDescripcion(),
+                        evento.getJuzgado() != null ? evento.getJuzgado().getNombre() : null,
+                        evento.getOficialia() != null ? evento.getOficialia().getNombre() : null
+                ))
+                .toList();
+
+        return new PageImpl<>(eventoRecords, pageable, eventosPage.getTotalElements());
+    }
+
+    @Transactional
+    public void deleteById(Integer id) {
+        if (!eventoRepository.existsById(id)) {
+            throw new IllegalArgumentException("El evento con id " + id + " no existe.");
+        }
+        eventoRepository.deleteById(id);
+    }
+
+    public EventoRecord editarEventoPeriodo(EventoEditRecord record) {
+        Evento evento = eventoRepository.findById(record.id())
+                .orElseThrow(() -> new NotFoundException("El evento no existe", "eventoId"));
+
+        if (record.diaInicio().isAfter(record.diaFin())) {
+            throw new DateTimeException("La fecha de inicio no puede ser mayor a la fecha de finalización");
+        }
+
+        evento
+                .setDescripcion(record.descripcion())
+                .setDiaInicio(record.diaInicio())
+                .setDiaFin(record.diaFin());
+        eventoRepository.save(evento);
+
+        return new EventoRecord(
+                evento.getId(),
+                evento.getDiaInicio(),
+                evento.getDiaFin(),
+                evento.getDescripcion(),
+                evento.getJuzgado() != null ? evento.getJuzgado().getNombre() : null,
+                evento.getOficialia() != null ? evento.getOficialia().getNombre() : null
+        );
     }
 }
--- src/test/java/mx/gob/pjpuebla/trials/core/eventos/EventoServiceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/core/eventos/EventoServiceTest.java
@@ -1,5 +1,10 @@
 package mx.gob.pjpuebla.trials.core.eventos;
 
+import mx.gob.pjpuebla.trials.core.eventos.records.EventoEditRecord;
+import mx.gob.pjpuebla.trials.core.eventos.records.EventoPeriodosRecord;
+import mx.gob.pjpuebla.trials.core.oficialias.Oficialia;
+import mx.gob.pjpuebla.trials.core.personas.Persona;
+import mx.gob.pjpuebla.trials.core.personas.PersonaService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,12 +15,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
 import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRepository;
 import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoSetUp;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class EventoServiceTest {
@@ -27,6 +38,9 @@ class EventoServiceTest {
 
     @Mock
     private JuzgadoRepository juzgadoRepository;
+
+    @Mock
+    PersonaService personaService;
 
     private Evento evento;
     private Juzgado juzgado;
@@ -110,4 +124,79 @@ class EventoServiceTest {
 
     }
 
+    @Test
+    void createPeriodosTest() {
+        EventoPeriodosRecord record = EventoSetUp.eventoPeriodosRecord();
+
+        Persona personaMock = new Persona();
+        given(personaService.getAuditor()).willReturn(personaMock);
+
+        given(eventoRepository.save(any(Evento.class))).willReturn(EventoSetUp.createEvento());
+
+        Evento eventoCreado = eventoService.createPeriodos(record);
+
+        assertThat(eventoCreado).isNotNull();
+        assertThat(eventoCreado.getDescripcion()).isEqualTo(record.descripcion());
+        assertThat(eventoCreado.getDiaInicio()).isEqualTo(record.diaInicio());
+        assertThat(eventoCreado.getDiaFin()).isEqualTo(record.diaFin());
+    }
+
+    @Test
+    void getEventosGeneralesTest() {
+        Evento eventoMock = EventoSetUp.createEvento();
+        Page<Evento> pageMock = new PageImpl<>(List.of(eventoMock), PageRequest.of(0, 10), 1);
+        given(eventoRepository.findEventosGenerales(PageRequest.of(0, 10))).willReturn(pageMock);
+
+        Page<EventoRecord> eventoRecords = eventoService.getEventosGenerales(PageRequest.of(0, 10));
+
+        assertThat(eventoRecords).isNotNull();
+        assertThat(eventoRecords.getContent()).hasSize(1);
+    }
+
+    @Test
+    void getEventosOficialiaComunTest() {
+        Oficialia oficialiaMock = new Oficialia();
+        Juzgado juzgadoMock = JuzgadoSetUp.createJuzgado();
+        Persona personaMock = new Persona();
+        personaMock.setOficialia(oficialiaMock);
+        personaMock.setJuzgado(juzgadoMock);
+        given(personaService.getAuditor()).willReturn(personaMock);
+
+        Evento eventoMock = EventoSetUp.createEvento();
+        Page<Evento> pageMock = new PageImpl<>(List.of(eventoMock), PageRequest.of(0, 10), 1);
+        given(eventoRepository.findByOficialiaOrJuzgado(oficialiaMock, juzgadoMock, PageRequest.of(0, 10))).willReturn(pageMock);
+
+        Page<EventoRecord> eventoRecords = eventoService.getEventosOficialiaComun(PageRequest.of(0, 10));
+
+        assertThat(eventoRecords).isNotNull();
+        assertThat(eventoRecords.getContent()).hasSize(1);
+    }
+
+    @Test
+    void deleteByIdTest() {
+        Integer eventoId = 1;
+        given(eventoRepository.existsById(eventoId)).willReturn(true);
+        eventoService.deleteById(eventoId);
+
+        verify(eventoRepository).existsById(eventoId);
+        verify(eventoRepository).deleteById(eventoId);
+    }
+
+    @Test
+    void editarEventoPeriodoTest() {
+        EventoEditRecord editRecord = EventoSetUp.eventoEditRecord();
+        Evento eventoExistente = EventoSetUp.createEvento();
+
+        given(eventoRepository.findById(editRecord.id())).willReturn(Optional.of(eventoExistente));
+        given(eventoRepository.save(any(Evento.class))).willReturn(eventoExistente);
+
+        EventoRecord resultado = eventoService.editarEventoPeriodo(editRecord);
+
+        assertThat(resultado).isNotNull();
+        assertThat(resultado.descripcion()).isEqualTo(editRecord.descripcion());
+        assertThat(resultado.diaInicio()).isEqualTo(editRecord.diaInicio());
+        assertThat(resultado.diaFin()).isEqualTo(editRecord.diaFin());
+
+        verify(eventoRepository).save(any(Evento.class));
+    }
 }
