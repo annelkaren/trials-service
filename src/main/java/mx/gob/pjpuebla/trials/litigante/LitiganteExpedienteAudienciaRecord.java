ade59ae0f14a7009f6e3fa2113325b061ba50647
Alex celisGuz <celisalex77@gmail.com>
2025-01-13 23:44:31 -0600

    Imports

--- src/main/java/mx/gob/pjpuebla/trials/litigante/LitiganteExpedienteAudienciaRecord.java
+++ src/main/java/mx/gob/pjpuebla/trials/litigante/LitiganteExpedienteAudienciaRecord.java
@@ -1,9 +1,6 @@
 package mx.gob.pjpuebla.trials.litigante;
 
-import mx.gob.pjpuebla.trials.workflow.audiencias.record.AudienciasExpedienteRecord;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record LitiganteExpedienteAudienciaRecord(
         Integer id,
--- src/main/java/mx/gob/pjpuebla/trials/workflow/audiencias/record/AudienciasExpedienteRecord.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/audiencias/record/AudienciasExpedienteRecord.java
@@ -1,7 +1,5 @@
 package mx.gob.pjpuebla.trials.workflow.audiencias.record;
 
-import java.time.LocalDateTime;
-
 public record AudienciasExpedienteRecord(
         Integer numeroAudiencia,
         String fechaInicio,
