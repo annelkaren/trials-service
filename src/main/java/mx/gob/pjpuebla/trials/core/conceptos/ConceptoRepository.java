766d691b2dfd567d434aaae898c51c3eb78cb4f2
Eduardo <eadweard132@gmail.com>
2025-01-15 00:08:46 -0600

    CRUD conceptos

--- src/main/java/mx/gob/pjpuebla/trials/core/conceptos/ConceptoRepository.java
+++ src/main/java/mx/gob/pjpuebla/trials/core/conceptos/ConceptoRepository.java
@@ -1,6 +1,10 @@
 package mx.gob.pjpuebla.trials.core.conceptos;
 
+import mx.gob.pjpuebla.trials.util.enums.Estado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +15,10 @@ public interface ConceptoRepository extends JpaRepository<Concepto, Integer> {
     Optional<Concepto> findByNombre(String nombre);
 
     List<Concepto> findAllByTipoJuicio_IdOrNombreIn(Integer tipoJuicioId, List<String> nombres);
+
+    @Query(value = "SELECT c FROM Concepto c "
+            + "LEFT JOIN FETCH c.tipoJuicio tj "
+            + "WHERE c.estado IN :estados "
+            + "AND (lower(c.nombre) LIKE %:key% OR LOWER(tj.nombre) LIKE %:key%)")
+    Page<Concepto> findAllConceptos(String key, List<Estado> estados, Pageable pageable);
 }
--- src/test/java/mx/gob/pjpuebla/trials/core/conceptos/ConceptoResourceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/core/conceptos/ConceptoResourceTest.java
@@ -2,6 +2,7 @@ package mx.gob.pjpuebla.trials.core.conceptos;
 
 import jakarta.ws.rs.core.MediaType;
 import mx.gob.pjpuebla.trials.error.NotFoundException;
+import mx.gob.pjpuebla.trials.util.enums.Estado;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,11 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.List;
-import static org.mockito.ArgumentMatchers.anyInt;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -83,4 +88,73 @@ class ConceptoResourceTest {
         ).andExpect(status().isBadRequest());
     }
 
+    @Test
+    void getAllConceptos_success() throws Exception {
+
+        ConceptoRecord conceptoRecord = new ConceptoRecord(1, "Distribución", 3, "Familiar (Tradicional)", Estado.ACTIVE);
+        List<ConceptoRecord> conceptosList = List.of(conceptoRecord);
+
+        given(mockConceptoService.getAllConceptos(any(Pageable.class), anyString()))
+                .willReturn(new PageImpl<>(conceptosList));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/core/conceptos/registros")
+                        .param("nombre", "nombre")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateStatus_success() throws Exception {
+        ConceptoRecord conceptoRecord = new ConceptoRecord(1, "Adjuntar", 1, "Tipo Juicio", Estado.ACTIVE);
+
+        given(mockConceptoService.updateStatus(anyInt(), anyInt())).willReturn(conceptoRecord);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/api/core/conceptos/1/status/0")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void delete_success() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/core/conceptos/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void create_success() throws Exception {
+        ConceptoRecord conceptoRecord = new ConceptoRecord(1, "Nuevo concepto", 1, "Tipo Juicio", Estado.ACTIVE);
+
+        given(mockConceptoService.createConcepto(any(Concepto.class))).willReturn(conceptoRecord);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/core/conceptos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"nombre\":\"Nuevo concepto\",\"dias\":1,\"estado\":\"ACTIVE\",\"tipoJuicio\":{\"id\":1,\"nombre\":\"Tipo Juicio\"}}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findByIdConceptoJuicio_success() throws Exception {
+        ConceptoRecordJuicio conceptoRecordJuicio = new ConceptoRecordJuicio(1, "Adjuntar", "Tipo Juicio", 1, Estado.ACTIVE, 1, 1);
+
+        given(mockConceptoService.findByConceptoById(anyInt())).willReturn(conceptoRecordJuicio);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/core/conceptos/conceptoJuicio/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void update_success() throws Exception {
+        ConceptoRecord conceptoRecord = new ConceptoRecord(1, "Concepto actualizado", 10, "Tipo Juicio", Estado.INACTIVE);
+
+        given(mockConceptoService.updateConcepto(any(Concepto.class))).willReturn(conceptoRecord);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/core/conceptos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":1,\"nombre\":\"Concepto actualizado\",\"dias\":10,\"estado\":\"INACTIVE\",\"tipoJuicio\":{\"id\":1,\"nombre\":\"Tipo Juicio\"}}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 }
