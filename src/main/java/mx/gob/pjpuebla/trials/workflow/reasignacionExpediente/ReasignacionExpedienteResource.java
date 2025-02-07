46ce59472ba14c3f162e306b0fd213ea35dcb0f6
alexis <alexxarellan@hotmail.com>
2025-02-06 18:01:20 -0600

    Modificación: En la reasignación del expediente ahora se guardan las personasDocumentos de la carpeta del expediente relacionado.

--- src/main/java/mx/gob/pjpuebla/trials/workflow/reasignacionExpediente/ReasignacionExpedienteResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/reasignacionExpediente/ReasignacionExpedienteResource.java
@@ -9,10 +9,6 @@ import lombok.RequiredArgsConstructor;
 import mx.gob.pjpuebla.trials.workflow.reasignacionExpediente.records.ReasignacionExpedienteResponseRecord;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 @RequiredArgsConstructor
 @RestController
--- src/main/java/mx/gob/pjpuebla/trials/workflow/reasignacionExpediente/ReasignacionExpedienteService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/reasignacionExpediente/ReasignacionExpedienteService.java
@@ -1,13 +1,11 @@
 package mx.gob.pjpuebla.trials.workflow.reasignacionExpediente;
 
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import lombok.RequiredArgsConstructor;
 import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
@@ -22,17 +20,16 @@ import mx.gob.pjpuebla.trials.error.NotFoundException;
 import mx.gob.pjpuebla.trials.util.enums.EstadoCarpeta;
 import mx.gob.pjpuebla.trials.util.enums.SelloEstatus;
 import mx.gob.pjpuebla.trials.util.enums.TipoCarpeta;
-import mx.gob.pjpuebla.trials.util.enums.TipoDocumento;
 import mx.gob.pjpuebla.trials.workflow.carpeta.Carpeta;
 import mx.gob.pjpuebla.trials.workflow.carpeta.CarpetaRepository;
 import mx.gob.pjpuebla.trials.workflow.documentos.Documento;
 import mx.gob.pjpuebla.trials.workflow.documentos.DocumentoRepository;
 import mx.gob.pjpuebla.trials.workflow.documentos.DocumentoService;
 import mx.gob.pjpuebla.trials.workflow.documentos.records.DocumentoData;
-import mx.gob.pjpuebla.trials.workflow.folios.DocumentoFoliosService;
 import mx.gob.pjpuebla.trials.workflow.movimientos.MovimientoService;
+import mx.gob.pjpuebla.trials.workflow.personasdocumentos.PersonaDocumento;
+import mx.gob.pjpuebla.trials.workflow.personasdocumentos.PersonaDocumentoRepository;
 import mx.gob.pjpuebla.trials.workflow.reasignacionExpediente.records.ReasignacionExpedienteResponseRecord;
-
 
 @Transactional
 @RequiredArgsConstructor
@@ -46,10 +43,11 @@ public class ReasignacionExpedienteService {
     private final DocumentoService documentoService;
     private final DocumentoRepository documentoRepository;
     private final MovimientoService movimientoService;
+    private final PersonaDocumentoRepository personaDocumentoRepository;
 
-    public ReasignacionExpedienteResponseRecord reasignarExpediente(Integer carpetaParentId){
+    public ReasignacionExpedienteResponseRecord reasignarExpediente(Integer carpetaParentId) {
         Persona auditor = personaService.getAuditor();
-        
+
         Oficialia oficialia = auditor.getOficialia();
         if (oficialia == null) {
             throw new NotFoundException("La persona no está relacionada con ninguna oficialía",
@@ -57,48 +55,55 @@ public class ReasignacionExpedienteService {
         }
 
         Carpeta carpetaParent = carpetaRepository.findById(carpetaParentId)
-                .orElseThrow(() -> new NotFoundException("Carpeta parent no encontrada", "carpetaId: " + carpetaParentId));
+                .orElseThrow(
+                        () -> new NotFoundException("Carpeta parent no encontrada", "carpetaId: " + carpetaParentId));
 
         TipoJuicio tipoJuicio = tipoJuicioRepository.findById(carpetaParent.getTipoJuicio().getId())
-        .orElseThrow(() -> new NotFoundException("Tipo de juicio no encontrado",
-                "tipoJuicioId: " + carpetaParent.getTipoJuicio().getId()));
+                .orElseThrow(() -> new NotFoundException("Tipo de juicio no encontrado",
+                        "tipoJuicioId: " + carpetaParent.getTipoJuicio().getId()));
 
         List<Juzgado> juzgadosRelacionados = juzgadoRepository.findJuzgadoByOficialiaId(oficialia.getId())
                 .stream().filter(j -> j.getTipoJuicios().contains(tipoJuicio)).toList();
 
         Juzgado juzgadoCarpetaNew = juzgadoService.getJuzgado(tipoJuicio, TipoCarpeta.DEMANDA, juzgadosRelacionados);
 
-        //Creación de carpeta.
+        // Creación de carpeta.
         Carpeta carpetaNew = new Carpeta()
-            .setTipoJuicio(tipoJuicio)
-            .setTipoCarpeta(TipoCarpeta.DEMANDA)
-            .setJuzgado(juzgadoCarpetaNew)
-            .setFolio(documentoService.getFolio("D"))
-            .setExpediente(documentoService.generateNumExpediente(juzgadoCarpetaNew, TipoCarpeta.DEMANDA))
-            .setEstatus(EstadoCarpeta.CAPTURA)
-            .setSelloEstatus(SelloEstatus.VALIDO)
-            .setPersona(auditor)
-            .setFechaAsignacion(LocalDateTime.now());
-        carpetaNew = carpetaRepository.save(carpetaNew);
-        
-        DocumentoData data = new DocumentoData()
-            .setCarpetaHistorica(carpetaParentId);
+                .setTipoJuicio(tipoJuicio)
+                .setTipoCarpeta(TipoCarpeta.DEMANDA)
+                .setJuzgado(juzgadoCarpetaNew)
+                .setFolio(documentoService.getFolio("D"))
+                .setExpediente(documentoService.generateNumExpediente(juzgadoCarpetaNew, TipoCarpeta.DEMANDA))
+                .setEstatus(EstadoCarpeta.CAPTURA)
+                .setSelloEstatus(SelloEstatus.VALIDO)
+                .setPersona(auditor)
+                .setFechaAsignacion(LocalDateTime.now());
+        Carpeta carpetaSaved = carpetaRepository.save(carpetaNew);
 
-        
+        // creación del documento
+        DocumentoData data = new DocumentoData()
+                .setCarpetaHistorica(carpetaParentId);
+
         Documento documento = new Documento()
-        .setCarpeta(carpetaNew)    
-        .setData(data)
-        .setPersona(auditor)
-        .setFechaAsignacion(LocalDateTime.now());
-       
+                .setCarpeta(carpetaNew)
+                .setData(data)
+                .setPersona(auditor)
+                .setFechaAsignacion(LocalDateTime.now());
         documento = documentoRepository.save(documento);
 
-        //TODO: Como se obtendrean los anexos y las personas relacionadas ? se registraran nuevos registros o se consultaran por los que ya existen ?
+        // TODO: Como se obtendrean los anexos
+
+        List<PersonaDocumento> personaDocumentos = personaDocumentoRepository.findByCarpetaId(carpetaParentId);
+        personaDocumentos.forEach(personaDocumento -> personaDocumento.setCarpeta(carpetaSaved));
+        
+        // Guardar todos los registros modificados de una sola vez
+        personaDocumentoRepository.saveAll(personaDocumentos);
 
         juzgadoService.actualizarCarga(carpetaNew.getJuzgado(), carpetaNew.getTipoCarpeta(), juzgadosRelacionados);
         movimientoService.createMovimento(carpetaNew, documento, auditor, null, EstadoCarpeta.CAPTURA.name());
-        
-        return new ReasignacionExpedienteResponseRecord(carpetaParentId, documento.getId(), "Carpeta reasignada exitosamente");
+
+        return new ReasignacionExpedienteResponseRecord(carpetaParentId, documento.getId(),
+                "Carpeta reasignada exitosamente");
     }
 
 }
