e2083668e845035fe594ba062042c9243e945c22
alexis <alexarellan96@hotmail.com>
2025-03-13 12:34:05 -0600

    ajuste para devolver  a OCP

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
@@ -925,42 +925,49 @@ public class DocumentoService {
         Page<Movimiento> page = movimientoService.getAllBandejaRecepcion(
                 pageable,
                 currentUser.getJuzgado().getId(),
-                Arrays.asList(EstadoCarpeta.TURNADO, EstadoCarpeta.RECEPCION),
+                List.of(EstadoCarpeta.TURNADO, EstadoCarpeta.RECEPCION),
                 key,
-                Arrays.asList(EstadoCarpeta.TURNADO.name(), EstadoCarpeta.RECEPCION.name()));
+                List.of(EstadoCarpeta.TURNADO.name(), EstadoCarpeta.RECEPCION.name()));
 
-        List<DocumentoBandejaRecepcionRecord> list = new ArrayList<>();
-
-        for (Movimiento movimiento : page.getContent()) {
-            Carpeta carpeta = movimiento.getCarpeta();
-            Documento documento = getDocumentoForRenderOficialMayor(movimiento, carpeta); // TODO: cambio en obtencion de documento para incluir apelación validar si es correcto el cambio (movimiento.getDocumento() != null) ? movimiento.getDocumento() : documentoRepository.findByCarpetaIdAndTipoDocumentoIsNull(carpeta.getId());
-          
-            boolean isPromocion = (documento != null && documento.getTipoDocumento() != null && documento.getTipoDocumento().equals(TipoDocumento.PROMOCION));
-            
-            String folio = (isPromocion) ? documento.getFolio() : carpeta.getFolio();
-            String tipoEntrada = (isPromocion)? etiquetaService.renderEtiquetaRecepcion("nuevoNombre", documento): etiquetaService.renderEtiquetaRecepcion("nuevoNombre", carpeta);
-            Map<String, Object> map = getOrigen(movimiento, currentUser);
-            String concepto = (isPromocion) ? documento.getConcepto().getNombre() : carpeta.getConcepto().getNombre();
-            
-            DocumentoBandejaRecepcionRecord drecord = new DocumentoBandejaRecepcionRecord(
-                    documento != null ? documento.getId() : null, // (isPromocion) ? documento.getId():carpeta.getId(),
-                                                                  // TODO: prueba para corregir anexos en la bandeja de
-                                                                  // recepcion.
-                    folio,
-                    (isPromocion) ? documento.getCarpeta().getExpediente() : carpeta.getExpediente(),
-                    StringUtils.capitalize(tipoEntrada.toLowerCase()),
-                    map.get("name").toString(),
-                    concepto,
-                    movimiento.getFechaAsignacion(),
-                    (Boolean) map.get(IS_INTERNO),
-                    null,
-                    null
-            );
-           
-
-            list.add(drecord);
-        }
-
+        List<DocumentoBandejaRecepcionRecord> list = page.getContent().stream()
+                .map(movimiento -> {
+                    Carpeta carpeta = movimiento.getCarpeta();
+                    Map<String, Object> map = getOrigen(movimiento, currentUser);
+                    Documento documento = getDocumentoForRenderOficialMayor(movimiento, carpeta);
+        
+                    boolean isPromocion = (documento != null && documento.getTipoDocumento() != null && documento.getTipoDocumento().equals(TipoDocumento.PROMOCION));
+                    String folio;
+                    String tipoEntrada;
+                    String concepto;
+                    String expediente;
+        
+                    if(isPromocion && documento != null){
+                        folio = documento.getFolio();
+                        tipoEntrada = etiquetaService.renderEtiquetaRecepcion("nuevoNombre", documento);
+                        concepto = documento.getConcepto().getNombre();
+                        expediente = documento.getCarpeta().getExpediente();
+                    }else{
+                        folio = carpeta.getFolio();
+                        tipoEntrada = etiquetaService.renderEtiquetaRecepcion("nuevoNombre", carpeta);
+                        concepto = carpeta.getConcepto().getNombre();
+                        expediente = carpeta.getExpediente();
+                    }
+        
+                    return  new DocumentoBandejaRecepcionRecord(
+                            documento != null ? documento.getId() : null, 
+                            folio,
+                            expediente,
+                            StringUtils.capitalize(tipoEntrada.toLowerCase()),
+                            map.get("name").toString(),
+                            concepto,
+                            movimiento.getFechaAsignacion(),
+                            (Boolean) map.get(IS_INTERNO),
+                            null,
+                            null
+                    );
+                
+                })
+                .toList();
 
         return new PageImpl<>(list, pageable, page.getTotalElements());
     }
