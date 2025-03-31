8a6c20da8d491fdd0ee6348246b586d1546b533d
alexis <alexarellan96@hotmail.com>
2025-03-31 14:17:12 -0600

    Ajuste turnado y devolución a oficialia

--- src/main/resources/db/migration/0.11/V0.11.1__alter_motivos.sql
+++ src/main/resources/db/migration/0.11/V0.11.1__alter_motivos.sql
@@ -1,3 +1,3 @@
 -- Modifica la columna estado para guardar cuando se retorna a oficialia. --
 ALTER TABLE TRIALS.TBL_MOVIMIENTOS 
-MODIFY COLUMN S_ESTADO VARCHAR(25);
+ALTER COLUMN S_ESTADO TYPE VARCHAR(25);
