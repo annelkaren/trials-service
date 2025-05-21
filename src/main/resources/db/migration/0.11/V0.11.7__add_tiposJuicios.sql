a9a09103f053a1ba68c7b579b88bad9312265388
alexis <alexarellan96@hotmail.com>
2025-05-21 13:46:08 -0600

    se agregan tipos de juicio faltantes para materia mercantil

---
+++ src/main/resources/db/migration/0.11/V0.11.7__add_tiposJuicios.sql
@@ -0,0 +1,15 @@
+INSERT INTO tbl_tipo_juicio (pn_id, n_version, s_nombre, n_estado, fn_tipo_sistema, fn_materia,
+                             s_usuario_alta, s_usuario_edita)
+VALUES (nextval('TRIALS.SEQ_TIPO_JUICIO_ID'), 0, 'Providencia precautoria', 0, 102, 200,
+        '6b13785f-d213-4585-a76b-437ffe57c9c7', '6b13785f-d213-4585-a76b-437ffe57c9c7');
+
+INSERT INTO tbl_tipo_juicio (pn_id, n_version, s_nombre, n_estado, fn_tipo_sistema, fn_materia,
+                             s_usuario_alta, s_usuario_edita)
+VALUES (nextval('TRIALS.SEQ_TIPO_JUICIO_ID'), 0, 'Medios preparatorios a Juicio', 0, 102, 200,
+        '6b13785f-d213-4585-a76b-437ffe57c9c7', '6b13785f-d213-4585-a76b-437ffe57c9c7');
+
+
+INSERT INTO tbl_tipo_juicio (pn_id, n_version, s_nombre, n_estado, fn_tipo_sistema, fn_materia,
+                             s_usuario_alta, s_usuario_edita)
+VALUES (nextval('TRIALS.SEQ_TIPO_JUICIO_ID'), 0, 'Oral Ejecutivo', 0, 101, 200,
+        '6b13785f-d213-4585-a76b-437ffe57c9c7', '6b13785f-d213-4585-a76b-437ffe57c9c7');
