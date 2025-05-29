4f38223ffa915fc6e0859322a9ab5c0a513e6794
Alejandro Ramírez <alejandro.ramirez@pjpuebla.gob.mx>
2025-05-29 17:05:48 -0600

    Corrige registro de expediente antiguo

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoService.java
@@ -1823,7 +1823,7 @@ public class DocumentoService {
                 carpeta.setJuzgado(persona.getJuzgado());
                 carpeta.setFolio(getFolio("D"));
                 carpeta.setTipoCarpeta(TipoCarpeta.DEMANDA);
-                carpeta.setExpediente(documentoRecord.numero() + "/" + documentoRecord.anio());
+                carpeta.setExpediente(String.format("%06d", Integer.parseInt(documentoRecord.numero()))  + "/" + documentoRecord.anio());
                 carpeta.setEstatus(EstadoCarpeta.ASIGNADO);
                 carpeta.setSelloEstatus(SelloEstatus.VALIDO);
                 carpeta.setFechaAsignacion(LocalDateTime.now());
@@ -1841,6 +1841,7 @@ public class DocumentoService {
                 createPersonaDocumento(documentoRecord.actor(), carpeta);
                 createPersonaDocumento(documentoRecord.demandado(), carpeta);
                 addAnexos(documentoRecord.anexos(), documento);
+                carpetaDetalleRepository.save(new CarpetaDetalle().setCarpeta(carpeta));
 
                 digitalizacionService.guardarArchivo(multipartFile, documento.getId());
 
