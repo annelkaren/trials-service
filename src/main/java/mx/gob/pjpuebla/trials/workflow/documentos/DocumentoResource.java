faf12e70c89ddbfed74ed18e640dd99b25731270
alexis <alexarellan96@hotmail.com>
2025-06-12 18:00:33 -0600

    CORRECCION SCAN

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoResource.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoResource.java
@@ -34,8 +34,6 @@ import java.io.IOException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
--- src/main/java/mx/gob/pjpuebla/trials/workflow/generadorQR/GeneradorQRService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/generadorQR/GeneradorQRService.java
@@ -17,7 +17,6 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
