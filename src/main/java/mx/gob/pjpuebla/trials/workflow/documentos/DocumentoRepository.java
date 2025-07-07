834642b91de6f4fb67f0c9af87b8d56596fed887
alexis <alexarellan96@hotmail.com>
2025-07-07 11:02:56 -0600

    ORDENAMIENTO SOLICITADO.

--- src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoRepository.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/documentos/DocumentoRepository.java
@@ -159,7 +159,10 @@ public interface DocumentoRepository extends JpaRepository<Documento, Integer>, 
                 OR (d IS NOT NULL AND :tipoEntradaDoc IS NOT NULL AND d.tipoDocumento = :tipoEntradaDoc)
                 OR (d IS NULL AND cd IS NOT NULL AND :tipoEntradaCarp IS NOT NULL AND cd.tipoCarpeta = :tipoEntradaCarp)
                 OR (d IS NULL AND cd IS NULL AND :tipoEntradaCarp IS NOT NULL AND c.tipoCarpeta = :tipoEntradaCarp)
-            )""")
+                
+            )
+            ORDER BY m.fechaAsignacion ASC 
+            """)
     Page<Movimiento> findByPersonaAsignada(String key, Integer juzgadoId, Persona personaAsignada, boolean isOficial,
             Pageable pageable, TipoCarpeta tipoCarpeta, TipoDocumento tipoDocumento, Integer folio,
             TipoDocumento tipoEntradaDoc, TipoCarpeta tipoEntradaCarp);
