22511c5300476d8c0e04390a54cd7903ba4cb41f
alexis <alexarellan96@hotmail.com>
2025-08-07 11:46:24 -0600

    Se incluye nombre del juez en los detalles del expediente.

--- src/main/java/mx/gob/pjpuebla/trials/workflow/carpeta/CarpetaService.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/carpeta/CarpetaService.java
@@ -9,6 +9,7 @@ import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
 import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoRepository;
 import mx.gob.pjpuebla.trials.core.oficialias.OficialiaRepository;
 import mx.gob.pjpuebla.trials.core.personas.Persona;
+import mx.gob.pjpuebla.trials.core.personas.PersonaRepository;
 import mx.gob.pjpuebla.trials.core.personas.PersonaService;
 import mx.gob.pjpuebla.trials.core.procedimientos.Procedimiento;
 import mx.gob.pjpuebla.trials.core.rubros.Rubro;
@@ -84,6 +85,7 @@ public class CarpetaService {
         private final JuzgadoRepository juzgadoRepository;
         private final OficialiaRepository oficialiaRepository;
         private final ConceptoRepository conceptoRepository;
+        private final PersonaRepository personaRepository;
 
         private static final String ACTOR_LABEL = "Actor";
         private static final String DEMANDADO_LABEL = "Demandado";
@@ -392,8 +394,6 @@ public class CarpetaService {
                 List<PersonaDataRecord> apelacionRecordResponseList = personaDocumentoRepository
                                 .findPersonaDocumentoDataByCarpetaId(carpeta.getId());
 
-                // Obtiene el nombre del juez TODO.obtener nombre del juez
-
                 DateTimeFormatter pattern = DateTimeFormatter.ofPattern(DATE_FORMAT);
 
                 EtapaProcesalRecord etapaProcesalRecord = null;
@@ -406,12 +406,17 @@ public class CarpetaService {
                                         carpetaEtapas.getEtapaProcesal().getNombre());
                 }
 
+                //obtenemos juzgado y nombre del juez :
+                Juzgado juzgado = carpeta.getJuzgado();
+                Persona juez = personaRepository.findByJuzgadoAndRolPrincipal(juzgado, "Juez").orElse(null);
+                String nombreJuez = juez != null ? juez.getNombre() + " " + juez.getApellidoPaterno() + " " + (juez.getApellidoMaterno() != null ? juez.getApellidoMaterno() : "") : "";
+                
                 return new InfoExpedienteRecord(
                                 carpeta.getExpediente(),
                                 carpeta.getTipoJuicio().getNombre(), // TODO mapear de forma correcta expediente tipo
                                                                      // PENAL
                                 carpeta.getTipoJuicio().getId(), // TODO mapear de forma correcta expediente tipo PENAL
-                                "", // extraAudienciaSelloRecord.nombreJuez(),
+                                nombreJuez, 
                                 carpeta.getAudit().getFechaAlta().format(pattern),
                                 "Asunto de penal desde Backend", // TODO añadir asunto para expediente tipo PENAL
                                 tipoProcedimiento,
