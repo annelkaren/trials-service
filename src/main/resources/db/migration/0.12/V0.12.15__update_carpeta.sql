54fae246a0b99950a270f80973389e71ca20d2af
alexis <alexarellan96@hotmail.com>
2025-09-22 11:23:03 -0600

    script para controlar los expedientes y documentos migrados .

---
+++ src/main/resources/db/migration/0.12/V0.12.15__update_carpeta.sql
@@ -0,0 +1,11 @@
+-- actualiza columna con 0 para indicar que todos los expedientes que se encuentran ya dados de alta en base no han sido migrados, son nativos del sistema.
+update trials.tbl_carpetas
+set n_migrado = 0
+where n_migrado is null;
+
+
+-- aplicamos lo mismo para documentos:
+
+update trials.tbl_documentos td 
+set n_migrado = 0
+where td.n_migrado is null;
