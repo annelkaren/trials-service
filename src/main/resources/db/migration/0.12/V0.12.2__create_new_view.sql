0b5e288081cc5208a6957dce237f30dc84b75666
Annel Gonzalez <annel.gonzalez1@outlook.com>
2025-09-24 09:53:54 -0600

    modificar campo de persona_detalle

--- src/main/resources/db/migration/0.12/V0.12.2__create_new_view.sql
+++ src/main/resources/db/migration/0.12/V0.12.2__create_new_view.sql
@@ -1,3 +1,7 @@
+-- se incrementa tamaño en nombre ya que se detecto en la migración que hay personas morales con nombres muy largos.
+ALTER TABLE TRIALS.TBL_PERSONAS_DOCUMENTOS
+ALTER COLUMN s_nombres TYPE VARCHAR(100);
+
 CREATE OR REPLACE VIEW trials.reporte_divorcios AS
 SELECT
   -- ===== DATOS DEL REGISTRO =====
