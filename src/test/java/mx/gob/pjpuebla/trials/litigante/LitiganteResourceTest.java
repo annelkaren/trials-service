662ada9b134a8cfc2bf3525e92e97a427588ab9a
alexis <alexarellan96@hotmail.com>
2025-11-11 17:21:06 -0600

    Ajuste tests.

--- src/test/java/mx/gob/pjpuebla/trials/litigante/LitiganteResourceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/litigante/LitiganteResourceTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Collections;
 import java.util.List;
 
--- src/test/java/mx/gob/pjpuebla/trials/litigante/LitiganteServiceTest.java
+++ src/test/java/mx/gob/pjpuebla/trials/litigante/LitiganteServiceTest.java
@@ -1,5 +1,6 @@
 package mx.gob.pjpuebla.trials.litigante;
 
+import mx.gob.pjpuebla.migracion.readers.usuario.UsuarioMigracionRepository;
 import mx.gob.pjpuebla.trials.core.juzgados.Juzgado;
 import mx.gob.pjpuebla.trials.core.juzgados.JuzgadoSetUp;
 import mx.gob.pjpuebla.trials.core.personas.Persona;
@@ -52,6 +53,8 @@ class LitiganteServiceTest extends SetupServiceTest {
     NotificacionRepository notificacionRepository;
     @Mock
     DocumentoRepository documentoRepository;
+    @Mock
+    UsuarioMigracionRepository usuarioMigracionRepository;
     @InjectMocks
     LitiganteService litiganteService;
 
