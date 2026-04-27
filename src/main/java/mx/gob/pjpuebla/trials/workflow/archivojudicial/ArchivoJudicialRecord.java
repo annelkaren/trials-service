1299cc48b82fb6bb18ae68facbf3bbe47d79a542
= <alexis.benitez@pjpuebla.gob.mx>
2026-04-27 09:28:27 -0600

    ajuste archivo judicial tipo de dato incompatible en spring boot 4

--- src/main/java/mx/gob/pjpuebla/trials/workflow/archivojudicial/ArchivoJudicialRecord.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/archivojudicial/ArchivoJudicialRecord.java
@@ -1,15 +1,14 @@
 package mx.gob.pjpuebla.trials.workflow.archivojudicial;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record ArchivoJudicialRecord(
-        Long id,
-        String juzgado,
-        String tipo,
-        String tipoId,
-        String expediente,
-        LocalDate fechaAlta,
-        String anexos,
-        Integer paquete
-) {
+                Long id,
+                String juzgado,
+                String tipo,
+                String tipoId,
+                String expediente,
+                LocalDateTime fechaAlta,
+                String anexos,
+                Integer paquete) {
 }
