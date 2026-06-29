839fc4cfc46b2ae93fd3fa5a74d7eb2309d19f18
Annel Gonzalez <annel.gonzalez1@outlook.com>
2026-06-28 19:41:57 -0600

    bandeja enviados

--- src/main/java/mx/gob/pjpuebla/trials/workflow/centralcomisarios/OficioCentralComisarioRepository.java
+++ src/main/java/mx/gob/pjpuebla/trials/workflow/centralcomisarios/OficioCentralComisarioRepository.java
@@ -1,6 +1,10 @@
 package mx.gob.pjpuebla.trials.workflow.centralcomisarios;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -8,4 +12,25 @@ public interface OficioCentralComisarioRepository extends JpaRepository<OficioCe
 
     Optional<OficioCentralComisario> findByDocumentoId(Integer oficioId);
 
+    @Query("""
+                SELECT new mx.gob.pjpuebla.trials.workflow.centralcomisarios.OficioEnviado(
+                    doc.id,
+                    COALESCE(c.expediente, 'N/A'),
+                    doc.folio,
+                    ins.nombre,
+                    dd.asunto,
+                    occ.estado,
+                    dd.fechaEmision,
+                    dd.fechaEntrega,
+                    occ.comisario.id
+                )
+                FROM OficioCentralComisario occ
+                JOIN occ.documento doc
+                LEFT JOIN doc.institucion ins
+                LEFT JOIN DocumentoDetalle dd ON dd.documento = doc
+                LEFT JOIN doc.carpeta c
+                WHERE doc.tipoDocumento =  mx.gob.pjpuebla.trials.util.enums.TipoDocumento.OFICIO
+                AND occ.comisario.id = :comisarioId
+            """)
+    Page<OficioEnviado> findDocumentosByComisario(@Param("comisarioId") Long comisarioId, Pageable pageable);
 }
